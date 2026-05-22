866d97d5e4539a7093ab8fa1342cfcfd6819da0d
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2026-05-22 11:04:41 +0700

    fix json logging

--- src/pertisk_eproxy_lager_json_formatter.erl
+++ src/pertisk_eproxy_lager_json_formatter.erl
@@ -11,7 +11,12 @@ format(Msg, Config) ->
     format(Msg, Config, []).
 
 format(Msg, _Config, _Colors) ->
-    [thoas:encode(build(Msg)), $\n].
+    try
+        [thoas:encode(build(Msg)), $\n]
+    catch
+        _:Reason ->
+            fallback_line(Msg, Reason)
+    end.
 
 build(Msg) ->
     MetaMap = metadata_map(lager_msg:metadata(Msg)),
@@ -62,11 +67,51 @@ encode_value(V) ->
     list_to_binary(io_lib:format("~p", [V])).
 
 timestamp_rfc3339(Msg) ->
-    St =
-        erlang:timestamp_to_system_time(lager_msg:timestamp(Msg), millisecond),
+    {{Y, Mo, D}, {H, Mi, S}} =
+        calendar:now_to_universal_time(lager_msg:timestamp(Msg)),
+    Micro = element(3, lager_msg:timestamp(Msg)),
     list_to_binary(
-        calendar:system_time_to_rfc3339(St, [{unit, millisecond}, {offset, "Z"}])
+        io_lib:format(
+            "~4..0w-~2..0w-~2..0wT~2..0w:~2..0w:~2..0w.~6..0wZ",
+            [Y, Mo, D, H, Mi, S, Micro]
+        )
     ).
+
+fallback_line(Msg, Reason) ->
+    {Date, Time} = lager_msg:datetime(Msg),
+    Level = level_bin(lager_msg:severity(Msg)),
+    Text = message_bin(Msg),
+    [
+        <<"{\"timestamp\":\"">>,
+        list_to_binary(Date),
+        <<"T">>,
+        list_to_binary(Time),
+        <<"Z\",\"level\":\"">>,
+        Level,
+        <<"\",\"message\":\"">>,
+        json_escape(Text),
+        <<"\",\"log_format_error\":\"">>,
+        json_escape(encode_value(Reason)),
+        <<"\"}">>,
+        $\n
+    ].
+
+json_escape(Bin) when is_binary(Bin) ->
+    << <<
+        (escape_char(C))
+     || <<C>> <= Bin
+    >> >>;
+json_escape(V) ->
+    json_escape(encode_value(V)).
+
+escape_char($") -> <<"\\\"">>;
+escape_char($\\) -> <<"\\\\">>;
+escape_char($\n) -> <<"\\n">>;
+escape_char($\r) -> <<"\\r">>;
+escape_char($\t) -> <<"\\t">>;
+escape_char(C) when C >= 32, C =< 126 -> <<C>>;
+escape_char(C) ->
+    unicode:characters_to_binary([C], utf8).
 
 level_bin(S) when is_atom(S) ->
     atom_to_binary(S, utf8);
