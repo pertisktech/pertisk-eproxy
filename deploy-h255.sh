b8bb07e2e54d101a7582ab4f1d29cd23473dd06c
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2026-05-27 10:58:03 +0700

    add deploy talos h255

---
+++ deploy-h255.sh
@@ -0,0 +1,11 @@
+# make docker-ingress-multi VERSION=0.3.46
+helm upgrade --install pertisk-eproxy ./deploy/helm/pertisk-eproxy -n pertisk-eproxy \
+  --set image.tag=0.3.46\
+  --set auth.username=admin \
+  --set auth.password='admin' \
+  --set auth0.domain=dev-od6cfzs2tugxm53g.us.auth0.com \
+  --set auth0.clientId=djuW8aR7VZQeS9SbW4ddnRCitgc6TiKO \
+  --set auth0.audience=https://dev-od6cfzs2tugxm53g.us.auth0.com/api/v2/ \
+  --set adminIngress.enabled=true \
+  --set adminIngress.host=admin.h255.thaidevops.co \
+  --set adminIngress.tlsSecretName=admin-h255-tls
