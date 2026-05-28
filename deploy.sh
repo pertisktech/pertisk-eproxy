70f092a76265ecca76345cd4b9d3cf2b5302a34d
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2026-05-28 10:27:15 +0700

    init upgrade quic

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-make docker-ingress-multi VERSION=0.3.81
+make docker-ingress-multi VERSION=0.4.0
 helm upgrade --install pertisk-eproxy ./deploy/helm/pertisk-eproxy -n pertisk-eproxy \
-  --set image.tag=0.3.81\
+  --set image.tag=0.4.0\
   --set auth.username=admin \
   --set auth.password='admin' \
   --set auth0.domain=dev-od6cfzs2tugxm53g.us.auth0.com \
