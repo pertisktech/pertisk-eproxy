9d4a22d2f0dab1ff409c23a0e2bc50113abdb1ab
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2026-06-14 16:20:57 +0700

    unit test - fix log level

--- test/pertisk_eproxy_log_level_tests.erl
+++ test/pertisk_eproxy_log_level_tests.erl
@@ -46,34 +46,10 @@ configured_invalid_env_falls_back_test() ->
     ?assert(is_atom(Level)).
 
 configured_defaults_when_unset_test() ->
-    application:ensure_all_started(lager),
-    case whereis(pertisk_eproxy_config) of
-        undefined -> {ok, _} = pertisk_eproxy_config:start_link();
-        _ -> ok
-    end,
     os:unsetenv("PERTISK_LOG_LEVEL"),
-    DbPath = pertisk_eproxy_test_helpers:tmp_db(),
-    file:delete(DbPath),
-    OldDb = application:get_env(pertisk_eproxy, db_file),
-    application:set_env(pertisk_eproxy, db_file, DbPath),
-    try
-        {ok, _} = pertisk_eproxy_db:init(DbPath),
-        Base = pertisk_eproxy_config:get_config(),
-        Config = maps:merge(maps:remove(log_level, Base), #{
-            sites => [],
-            backends => [],
-            certificates => [],
-            dns_providers => []
-        }),
-        ok = pertisk_eproxy_test_helpers:put_config_retry(Config),
+    with_mocked_config(#{http_port => 80}, fun() ->
         ?assertEqual(info, pertisk_eproxy_log_level:configured())
-    after
-        case OldDb of
-            {ok, V} -> application:set_env(pertisk_eproxy, db_file, V);
-            undefined -> application:unset_env(pertisk_eproxy, db_file)
-        end,
-        file:delete(DbPath)
-    end.
+    end).
 
 parse_atom_levels_test() ->
     ?assertEqual({ok, debug}, pertisk_eproxy_log_level:parse(debug)),
@@ -129,30 +105,18 @@ apply_empty_handlers_covers_backend_error_paths_test_body() ->
     end.
 
 configured_from_config_log_level_test() ->
-    application:ensure_all_started(lager),
-    case whereis(pertisk_eproxy_config) of
-        undefined ->
-            {ok, _} = pertisk_eproxy_config:start_link();
-        _ ->
-            ok
-    end,
-    OldMode = os:getenv("PERTISK_MODE"),
-    os:putenv("PERTISK_MODE", "proxy"),
     os:unsetenv("PERTISK_LOG_LEVEL"),
-    Base = pertisk_eproxy_config:get_config(),
-    Config = maps:merge(Base, #{log_level => notice}),
+    with_mocked_config(#{log_level => notice}, fun() ->
+        ?assertEqual(notice, pertisk_eproxy_log_level:configured())
+    end).
+
+with_mocked_config(Config, Fun) ->
+    meck:new(pertisk_eproxy_config, [passthrough, unstick]),
     try
-        case pertisk_eproxy_test_helpers:put_config_retry(Config) of
-            ok ->
-                ?assertEqual(notice, pertisk_eproxy_log_level:configured());
-            {error, _} ->
-                ok
-        end
+        meck:expect(pertisk_eproxy_config, get_config, fun() -> Config end),
+        Fun()
     after
-        case OldMode of
-            false -> os:unsetenv("PERTISK_MODE");
-            V -> os:putenv("PERTISK_MODE", V)
-        end
+        pertisk_eproxy_test_helpers:unload_mocks([pertisk_eproxy_config])
     end.
 
 apply_file_backend_ok_path_test() ->
